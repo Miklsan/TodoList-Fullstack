0f1633c2806ab01109929354e4e0b063db2fbebc
Miklsan <mborghmans@gmail.com>
2020-10-09 14:37:43 +0200

    tests loginForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react"
+import React,{useState} from "react"
 import ModeSwitch from "./components/ModeSwitch"
 import Todos from "./components/Todos"
 import ModeContextProvider from "./Context/ModeContext"
 import AppContainer from "./components/AppContainer"
 import date from "./components/Date"
 import LoginForm from "./components/LoginForm"
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [username, setUsername] = useState('')
+  const handleLogoutClick = () => {
+    setUsername('')
+    setLoggedIn(false)}
   return (
     <ModeContextProvider>
       <AppContainer>
@@ -16,8 +22,13 @@ function App() {
           <ModeSwitch />
           <Todos />
         </div>
-        <h2 className="text-center">Login</h2>
+        <div>
+        <h2 className="text-center mb-3">Login</h2>
         <LoginForm />
+        <button type="button" className="btn btn-danger">
+          Log out
+        </button>
+        </div>
       </AppContainer>
     </ModeContextProvider>
   )
--- src/components/Date.js
+++ src/components/Date.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 
 
 
-moment.locale('fr')
+moment.locale('fr');
 const date = moment().format('dddd Do MMMM, h:mm:ss a')
 
 console.log(date)
